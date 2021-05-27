edd3927c80dd4bd427bbc90f3ad7f432bd4f416d
AntLinux <anton-r@live.com>
2021-05-27 21:28:32 +0300

    db adons

---
+++ ecomCore/settings/__init__.py
@@ -0,0 +1,13 @@
+
+import sys
+from .base import *
+
+RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
+if RUNNING_DEVSERVER:
+    from .local import *
+else:
+    from .server import *
+
+"""
+dont forget to add BASE_DIR parent dir !! because of different settings 
+"""
