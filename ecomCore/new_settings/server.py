316e365c82c4d2e914a8a1d5628ac8319b462a33
AntLinux <anton-r@live.com>
2021-05-27 20:51:19 +0300

    static added

---
+++ ecomCore/new_settings/server.py
@@ -0,0 +1,14 @@
+
+# must be Debug = False but for static files i live it like this
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dckh9bns3abpbh',
+        'USER': 'srpkndhjrssvge',
+        'PASSWORD': '4118c271e25ac5460a6f96e83e1454df2eb9d0b37d08858f1f57fc5be755fcfd',
+        'HOST': 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com',
+        'POST': '',
+    }
+}
