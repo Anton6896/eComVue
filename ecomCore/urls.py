717f687a256b261a16985bee62836909288ed223
AntLinux <anton-r@live.com>
2021-05-27 21:39:58 +0300

    ff

--- ecomCore/urls.py
+++ ecomCore/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile/', include('profile_user.urls')),
+    path('', include('profile_user.urls')),
 
 
 
