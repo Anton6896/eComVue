4a76d5d6682474ded2753300752106e1d9684608
AntLinux <anton-r@live.com>
2021-06-05 18:47:10 +0300

    os path added secret key value

--- ecomCore/settings/base.py
+++ ecomCore/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*45ufiux=ha8m#e*0%tl#j)!l_^@7y+7jz0re8*2#7ps!yi78'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
